b271d6cff808f6828953a2eef6a3865dd1ce4f65
fadrigo <kurtdavid.fadrigo@wvsu.edu.ph>
2024-10-01 10:30:25 +0800

    initial_signup

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:cc206_mealplanner/features/signup.dart'; 
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const SignUpPage(), // Use SignUpPage here
+    );
+  }
+}
